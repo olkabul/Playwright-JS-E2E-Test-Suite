5d3ea0375aa91abef9c5715df28fc5d735ee3834
olkabul <olga.bulgakova@gmail.com>
2025-06-15 12:08:01 +0300

    Initial: create project and structure

---
+++ playwright.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  timeout: 30000,
+  use: {
+    browserName: "chromium",
+    headless: false,
+    viewport: { width: 1600, height: 1000 },
+  },
+});
