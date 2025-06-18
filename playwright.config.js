0fcd5aa800c8857c598b9ee98724d5fe2ebfee55
olkabul <olga.bulgakova@gmail.com>
2025-06-18 16:56:24 +0300

    git actions setup 19

--- playwright.config.js
+++ playwright.config.js
@@ -6,9 +6,10 @@ import path from "path";
 dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 const isCI = !!process.env.CI;
+const testType = process.env.TEST_TYPE || "ui"; // default to UI if not specified
 
-export default defineConfig({
-  testDir: "./tests",
+//Base config
+const config = {
   timeout: 30000,
   use: {
     screenshot: "only-on-failure",
@@ -17,8 +18,18 @@ export default defineConfig({
     browserName: "chromium",
     headless: isCI,
     viewport: { width: 1600, height: 1000 },
-    storageState: "auth.json",
   },
-  globalSetup: "./tests/helpers/ui-login.js",
   reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
-});
+};
+
+// Adjusting to test type
+if (testType === "ui") {
+  config.testDir = "./tests/ui-tests";
+  config.globalSetup = "./tests/helpers/ui-login.js";
+  config.use.storageState = "auth.json";
+} else if (testType === "api") {
+  config.testDir = "./tests/api-tests";
+  // No UI login or storageState needed
+}
+
+export default defineConfig(config);
